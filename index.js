f46b83c1c8cc448dafcf2a8ebea5a7f0f0957d2f
piesieomane <piesieyeboah@gmail.com>
2022-05-21 19:18:15 +0300

    Set the reset button

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ let computerScore = 0
 
 let sp1 = document.getElementById("sp1");
 let sp2 = document.getElementById("sp2");
+const reset = document.getElementById("reset");
+reset.addEventListener("click", reserter)
+
+function reserter() {
+  playerScore = 0;
+  computerScore = 0;
+  sp1.innerHTML = "Player";
+  sp2.innerHTML = "Computer";
+
+}
 
 function getComputerSelection() {
     return  selection[ Math.floor( Math.random() * 3 ) ]  
@@ -16,8 +26,8 @@ const triggerButtons = Array.from(trigger)
 triggerButtons.forEach(button => {
     button.onclick = function (event) {
         console.log("event", event)
-        const userSelection = event.target.parentElement.getAttribute("id")
-        const computerSelection = getComputerSelection()
+      const userSelection = event.target.parentElement.getAttribute("id");
+      const computerSelection = getComputerSelection();
         
         console.log("selections",userSelection, computerSelection)
         playground(computerSelection, userSelection)
@@ -26,14 +36,24 @@ triggerButtons.forEach(button => {
 
         sp1.textContent = playerScore
         sp2.textContent = computerScore
+      
 
         if (computerScore === 5) {
             roundWinner.textContent = 'Winner is computer'
-            alert("Game ended")
+            //alert("Game ended")
+          sp2.innerHTML = "5"
+          setTimeout(() => {
+            reserter();
+          }, 3000);
+         
             return
           } else if (playerScore === 5) {
             roundWinner.textContent = 'Winner is Player'
-            alert("Game ended")
+            //alert("Game ended")
+            sp1.innerHTML = "5"
+            setTimeout(() => {
+              reserter();
+            }, 3000);
             return
           }
     }
